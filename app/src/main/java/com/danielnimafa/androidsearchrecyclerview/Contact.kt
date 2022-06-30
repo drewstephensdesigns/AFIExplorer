6af4b1c840b8f5c6fd0644c38829989679dac3f9
DrewStephensCoding <usafcrewchief06@yahoo.com>
2022-06-30 05:32:25 -0500

    test

--- app/src/main/java/com/danielnimafa/androidsearchrecyclerview/Contact.kt
+++
@@ -1,8 +0,0 @@
-package com.danielnimafa.androidsearchrecyclerview
-
-/**
- * Created by danielnimafa on 16/03/18.
- */
-data class Contact(var name: String? = "",
-                   var image: String? = "",
-                   var phone: String? = ""  )
--- app/src/test/java/com/drewcodesit/afiexplorer/ExampleUnitTest.kt
+++ app/src/test/java/com/drewcodesit/afiexplorer/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
-package com.danielnimafa.androidsearchrecyclerview
+package com.drewcodesit.afiexplorer
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).

2c212cfadf5b51b6b63344b6b3ac3b2484cd008f
DrewStephensCoding <usafcrewchief06@yahoo.com>
2022-07-29 13:49:53 -0500

    test

---
+++ app/src/main/java/com/drewcodesit/afiexplorer/utils/Config.kt
@@ -0,0 +1,15 @@
+package com.drewcodesit.afiexplorer.utils
+
+object Config {
+
+    // API
+    const val BASE_URL = "https://api.afiexplorer.com/"
+
+    // Database
+    const val DATABASE_NAME = "myfavdb"
+
+    // Table
+    const val TABLE_NAME = "favoriteslist"
+
+
+}

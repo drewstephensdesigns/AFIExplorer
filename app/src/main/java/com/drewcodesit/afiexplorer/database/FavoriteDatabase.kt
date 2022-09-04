44711fc067dbd007629eeea7ac6202504d493e38
DrewStephensCoding <usafcrewchief06@yahoo.com>
2022-09-04 10:22:47 -0500

    Bug fixes

--- app/src/main/java/com/drewcodesit/afiexplorer/database/FavoriteDatabase.kt
+++ app/src/main/java/com/drewcodesit/afiexplorer/database/FavoriteDatabase.kt
@@ -2,26 +2,30 @@ package com.drewcodesit.afiexplorer.database
 
 import android.content.Context
 import android.util.Log
-import androidx.room.AutoMigration
-import androidx.room.Database
-import androidx.room.Room
-import androidx.room.RoomDatabase
+import androidx.room.*
 import com.drewcodesit.afiexplorer.utils.Config
+import java.io.File
 
 @Database
     (entities = [FavoriteEntity::class],
-    version = 2,
+    version = 3,
     autoMigrations = [
-        AutoMigration (from = 1, to = 2)
+        AutoMigration (from = 2, to = 3)
     ],
     exportSchema = true)
 abstract class FavoriteDatabase : RoomDatabase() {
+    @RenameTable(fromTableName = "favoritelist", toTableName = Config.TABLE_NAME)
 
     companion object {
         @Volatile
         var INSTANCE: FavoriteDatabase? = null
 
         fun getDatabase(context: Context): FavoriteDatabase {
+
+            // TEST
+            //val dbFile = context.applicationContext.getDatabasePath("favoritelist.db")
+            //if(dbFile.exists()) dbFile.renameTo(File(dbFile.path + Config.TABLE_NAME))
+
             if (INSTANCE == null) synchronized(FavoriteDatabase::class.java){
                 INSTANCE = Room.databaseBuilder(
                     context.applicationContext,

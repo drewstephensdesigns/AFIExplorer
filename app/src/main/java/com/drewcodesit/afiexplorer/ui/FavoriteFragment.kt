8123236c0f81ae9df1b1a760e43372e1e6ab9889
DrewStephensCoding <usafcrewchief06@yahoo.com>
2023-06-25 20:48:18 -0500

    Complete rebuild

---
+++ app/src/main/java/com/drewcodesit/afiexplorer/ui/FavoriteFragment.kt
@@ -0,0 +1,228 @@
+package com.drewcodesit.afiexplorer.ui
+
+import android.app.SearchManager
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.drewcodesit.afiexplorer.R
+import com.drewcodesit.afiexplorer.adapters.FavesAdapter
+import com.drewcodesit.afiexplorer.database.FavoriteDatabase
+import com.drewcodesit.afiexplorer.database.FavoriteEntity
+import com.drewcodesit.afiexplorer.databinding.FragmentFavesBinding
+import com.drewcodesit.afiexplorer.utils.MyDividerItemDecoration
+import com.maxkeppeler.sheets.core.ButtonStyle
+import com.maxkeppeler.sheets.core.IconButton
+import com.maxkeppeler.sheets.core.SheetStyle
+import com.maxkeppeler.sheets.info.InfoSheet
+import com.maxkeppeler.sheets.input.InputSheet
+import com.maxkeppeler.sheets.input.type.InputRadioButtons
+import com.rajat.pdfviewer.PdfViewerActivity
+import es.dmoral.toasty.Toasty
+
+class FavoriteFragment : Fragment(),
+    FavesAdapter.FavesSelectedListener ,
+    FavesAdapter.FavesDeletedListener,
+    MenuProvider {
+
+    private var _binding: FragmentFavesBinding? = null
+    private val binding get() = _binding!!
+    private var favesAdapter: FavesAdapter? = null
+    private var searchViewFaves: SearchView? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentFavesBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupMenuFaves()
+        initUI()
+        fetchFaves()
+        setupBottomMenu()
+
+    }
+
+    private fun setupMenuFaves(){
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(
+            this,           // your Fragment implements MenuProvider, so we use this here
+            viewLifecycleOwner,     // Only show the Menu when your Fragment's View exists
+            Lifecycle.State.RESUMED // And when the Fragment is Created
+        )
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menu.clear()
+        menuInflater.inflate(R.menu.menu_faves, menu)
+        val searchManager =
+            requireActivity().getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchViewFaves = menu.findItem(R.id.action_search_faves)?.actionView as SearchView
+        searchViewFaves?.apply {
+            setSearchableInfo(searchManager.getSearchableInfo(requireActivity().componentName))
+            setIconifiedByDefault(true)
+            maxWidth = Int.MAX_VALUE
+        }
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        return when(menuItem.itemId){
+            R.id.action_search_faves ->{
+                true
+            }
+            R.id.action_clear_database ->{
+                nukeDatabase()
+                true
+            }
+            else -> false
+        }
+    }
+
+    private fun initUI(){
+        binding.rvFavorites.layoutManager = LinearLayoutManager(context)
+        binding.rvFavorites.setHasFixedSize(true)
+        binding.rvFavorites.apply {
+            itemAnimator = DefaultItemAnimator()
+            addItemDecoration(
+                MyDividerItemDecoration(
+                    context,
+                    DividerItemDecoration.VERTICAL,
+                    36
+                )
+            )
+        }
+    }
+
+    private fun fetchFaves(){
+        val favorites = FavoriteDatabase.getDatabase(requireContext()).favoriteDAO()?.getFavoriteData()
+
+        Log.i("---- FETCHING FAVORITES", "$favorites")
+
+        favesAdapter = FavesAdapter(requireContext(), favorites!! , this, this)
+        binding.rvFavorites.adapter = favesAdapter
+
+        // Show or Hide Empty State
+        if (favorites.isEmpty()) {
+            // Kotlin View Binding
+            binding.emptyFavesInfoImg.visibility = View.VISIBLE
+            binding.emptyFavesInfoText.visibility = View.VISIBLE
+            binding.emptyFavesInfoText.text = getString(R.string.no_results_found_db)
+            binding.fabFilterFaves.hide()
+
+        } else {
+            Log.i("FAVORITES", "Current Size: ${favorites.size}")
+            // Kotlin View Binding
+            binding.emptyFavesInfoImg.visibility = View.GONE
+            binding.emptyFavesInfoText.visibility = View.GONE
+            if(favorites.size > 5){
+                binding.fabFilterFaves.show()
+            } else {
+                binding.fabFilterFaves.hide()
+            }
+            favesAdapter?.filterByTitle()
+        }
+    }
+
+    // Allows user to sort favorites by title or number
+    private fun setupBottomMenu() {
+        binding.fabFilterFaves.setOnClickListener {
+            InputSheet().show(requireContext()){
+                title("Sort By")
+                with(InputRadioButtons {
+                    options(mutableListOf("Publication Title", "Publication Number"))
+                    changeListener { value ->
+                        when(value){
+                            0 -> {favesAdapter?.filterByTitle()}
+                            1 -> {favesAdapter?.filterByNumber()}
+                        }
+                    }
+                })
+            }
+        }
+    }
+
+    // Delete Entire Database
+    private fun nukeDatabase(){
+        InfoSheet().show(requireContext()){
+            style(SheetStyle.DIALOG)
+            withIconButton(IconButton( R.drawable.ic_error)){}
+            title("Delete Favorites?")
+            content("This will clear the database, are you sure you want to continue?")
+            onNegative(
+                "Not yet",
+            ) { /* Set listener when negative button is clicked. */ }
+            onPositive("Yes") {
+                FavoriteDatabase.getDatabase(requireContext()).favoriteDAO()?.deleteAll()
+                fetchFaves()
+                showDeleteToast("Database cleared!")
+            }
+            positiveButtonStyle(ButtonStyle.OUTLINED)
+            negativeButtonStyle(ButtonStyle.NORMAL)
+            displayNegativeButton(true)
+            displayPositiveButton(true)
+        }
+    }
+
+    // Opens document from the Favorites Screen
+    // If PDF Reader installed the doc will open natively
+    // Else falls back to the PDFViewer Activity
+    override fun onFavesSelectedListener(faves: FavoriteEntity) {
+        try {
+            val intent = Intent(Intent.ACTION_VIEW)
+            intent.setDataAndType(Uri.parse(faves.DocumentUrl), "application/pdf")
+            startActivity(intent)
+        } catch (e: ActivityNotFoundException) {
+            startActivity(
+                // Use 'launchPdfFromPath' if you want to use assets file (enable "fromAssets" flag) / internal directory
+                PdfViewerActivity.launchPdfFromUrl(      //PdfViewerActivity.Companion.launchPdfFromUrl(..   :: incase of JAVA
+                    requireContext(),
+                    "${faves.DocumentUrl}",       // PDF URL in String format
+                    "${faves.Number}",           // PDF Name/Title in String format
+                    "",                     // If nothing specific, Put "" it will save to Downloads
+                    enableDownload = true                // This param is true by default.
+                )
+            )
+        }
+    }
+
+    // Deletes single row from database and refreshes the list
+    override fun onFavesDeletedListener(faveSingleItemDelete: FavoriteEntity, position: Int) {
+        FavoriteDatabase.getDatabase(requireContext()).favoriteDAO()?.delete(faveSingleItemDelete)
+        showDeleteToast("You deleted ${faveSingleItemDelete.Number}")
+        favesAdapter?.notifyItemRemoved(position)
+        //favesAdapter?.notifyItemChanged(position)
+        fetchFaves()
+    }
+
+    private fun showDeleteToast(message: String){
+        Toasty.info(requireContext(), message, R.drawable.ic_error).show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
---
+++ app/src/main/java/com/drewcodesit/afiexplorer/ui/HomeFragment.kt
@@ -0,0 +1,309 @@
+package com.drewcodesit.afiexplorer.ui
+
+import android.app.SearchManager
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
+import androidx.annotation.RequiresApi
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.drewcodesit.afiexplorer.R
+import com.drewcodesit.afiexplorer.adapters.PubsAdapter
+import com.drewcodesit.afiexplorer.databinding.FragmentHomeBinding
+import com.drewcodesit.afiexplorer.model.Pubs
+import com.drewcodesit.afiexplorer.utils.MyDividerItemDecoration
+import com.drewcodesit.afiexplorer.view.MainActivity
+import com.drewcodesit.afiexplorer.viewModel.PubsViewModel
+import com.maxkeppeler.sheets.input.InputSheet
+import com.maxkeppeler.sheets.input.type.InputRadioButtons
+import com.maxkeppeler.sheets.input.type.spinner.InputSpinner
+import com.rajat.pdfviewer.PdfViewerActivity
+import es.dmoral.toasty.Toasty
+
+
+class HomeFragment : Fragment(),
+    PubsAdapter.PubsAdapterListener,
+    MenuProvider {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private var pubsViewModel: PubsViewModel? = null
+    private var adapter: PubsAdapter? = null
+    private var searchView: SearchView? = null
+
+    private var exit: Boolean = false
+
+    //The OnBackPressedDispatcher is a class that allows you
+    // to register a OnBackPressedCallback to a LifecycleOwner
+    private val onBackPressedCallback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+        @RequiresApi(Build.VERSION_CODES.N)
+        override fun handleOnBackPressed() {
+            closeOrRefreshApp()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        // Loading Indicator
+        binding.loading.visibility = View.VISIBLE
+
+        // Separates business logic from the UI
+        pubsViewModel = ViewModelProvider(requireActivity(),
+            ViewModelProvider.AndroidViewModelFactory
+                .getInstance(requireActivity().application))[PubsViewModel::class.java]
+
+        pubsViewModel?.publications?.observe(viewLifecycleOwner) { pubsList ->
+            pubsList?.let {
+                adapter = PubsAdapter(requireContext(), it, this)
+                _binding?.recyclerView?.adapter = adapter
+                binding.loading.visibility = View.GONE
+            }
+        }
+        return binding.root
+    }
+
+    // An override in a Fragment class that gets called when the associated
+    // view has been created. It performs necessary UI initialization
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initUI()
+        setupMenu()
+        setupBottomSheet()
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, onBackPressedCallback)
+    }
+
+    // While the Fragment menu API, which could be used for creating menu items and
+    // reacting to the selection of such, is now marked as deprecated, the ComponentActivity
+    // replaces these functionalities by implementing the MenuHost interface.
+    // Source: https://medium.com/tech-takeaways/how-to-migrate-the-deprecated-oncreateoptionsmenu-b59635d9fe10
+    private fun setupMenu() {
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(
+            this,           // your Fragment implements MenuProvider, so we use this here
+            viewLifecycleOwner,     // Only show the Menu when your Fragment's View exists
+            Lifecycle.State.CREATED // And when the Fragment is Created
+        )
+    }
+
+    // the code first inflates the menu. It then obtains a reference to the SearchManager system service
+    // and creates a SearchView widget by retrieving the MenuItem with the ID R.id.action_search from the
+    // menu and casting its action view to a SearchView. Then configured to use the SearchableInfo obtained
+    // from the SearchManager. Finally, an OnQueryTextListener is set on the SearchView to listen for changes
+    // in the query text. When the query text changes, the adapter's filter is applied with the new text
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        menuInflater.inflate(R.menu.menu_search, menu)
+        val searchManager =
+            requireActivity().getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView = menu.findItem(R.id.action_search)?.actionView as SearchView
+
+        searchView?.apply {
+            setSearchableInfo(searchManager.getSearchableInfo(requireActivity().componentName))
+            setIconifiedByDefault(true)
+            maxWidth = Int.MAX_VALUE
+
+            setOnQueryTextListener(object : SearchView.OnQueryTextListener{
+                override fun onQueryTextSubmit(query: String?): Boolean {
+                    searchView?.clearFocus()
+                    return true
+                }
+
+                override fun onQueryTextChange(newText: String?): Boolean {
+                    adapter?.filter?.filter(newText)
+                    when (adapter?.itemCount) {
+                        0 -> {
+                            // Kotlin View Binding
+                            _binding?.noResultsFound?.visibility = View.VISIBLE
+                            _binding?.noResultsFound?.text = getString(R.string.no_results_found, newText)
+                            _binding?.recyclerView?.visibility = View.GONE
+                        }
+                        else -> {
+                            _binding?.noResultsFound?.visibility = View.GONE
+                            _binding?.recyclerView?.visibility = View.VISIBLE
+                        }
+                    }
+                    return false
+                }
+            })
+        }
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        // Validate and handle the selected menu item
+        return when (menuItem.itemId) {
+            R.id.action_search -> {
+                true
+            }
+            else -> {
+                false
+            }
+        }
+    }
+
+    // Sets up the RecyclerView with a layout manager, fixed size, animation, and a
+    // custom divider between items.
+    private fun initUI() {
+        binding.recyclerView.layoutManager = LinearLayoutManager(context)
+        binding.recyclerView.setHasFixedSize(true)
+        binding.recyclerView.apply {
+            itemAnimator = DefaultItemAnimator()
+            addItemDecoration(
+                MyDividerItemDecoration(
+                    context,
+                    DividerItemDecoration.VERTICAL,
+                    36
+                )
+            )
+        }
+    }
+
+    // Displays bottom sheet for filtering of publications
+    // Radio Buttons for non Major Commands
+    // Spinner Selection for Major Commands
+    private fun setupBottomSheet() {
+       binding.fabFilterMain.setOnClickListener {
+            InputSheet().show(requireContext()){
+                title("Filter Publications")
+
+                // Input buttons for non-epubs/MAJCOM publications
+                // DoD: JTR, GTC, DTS regs
+                // AF/: All HAF level publications (SAF, AF, JAG, etc)
+                // LeMay Center: Air Force Doctrine (TTPs are restricted)
+                with(InputRadioButtons {
+                    label("Select an Organization")
+                    options(mutableListOf("DoD", "HAF", "LeMay Center"))
+                    changeListener { value ->
+                        when(value){
+                            0 -> {updateFilter("DoD", "DoD")}
+                            1 -> {updateFilter("AF/", "HAF")}
+                            2 -> {updateFilter("LeMay", "Doctrine")}
+                        }
+                    }
+                })
+
+                // Input Spinner of Major Commands for cleaner look
+                with(InputSpinner {
+                    label("Select a Command from the dropdown")
+                    options(
+                        mutableListOf(
+                            "ACC", "AMC",
+                            "AETC", "PACAF",
+                            "USAFE-AFAFRICA",
+                            "AFGSC", "AFMC",
+                            "AFRC", "AFSOC"
+                        )
+                    )
+                    changeListener { value ->
+                        when(value){
+                            0 -> {updateFilter("ACC", "ACC")}
+                            1 -> {updateFilter("AMC", "AMC")}
+                            2 -> {updateFilter("AETC", "AETC")}
+                            3 -> {updateFilter("PACAF", "PACAF")}
+                            4 -> {updateFilter("USAFE-AFAFRICA", "USAFE-AFAFRICA")}
+                            5 -> {updateFilter("AFGSC", "AFGSC")}
+                            6 -> {updateFilter("AFMC", "AFMC")}
+                            7 -> {updateFilter("AFRC", "AFRC")}
+                            8 -> {updateFilter("AFSOC", "AFSOC")}
+                        }
+                    }
+                })
+            }
+       }
+    }
+
+    // Change ToolBar Title: (activity as MainActivity).supportActionBar?.title = ""
+    // Source: https://stackoverflow.com/questions/27100007/set-a-title-in-toolbar-from-fragment-in-android
+    private fun updateFilter(org: String, title: String) {
+        adapter?.filterByRescindOrg()?.filter(org)
+        (activity as MainActivity).supportActionBar?.title = title
+    }
+
+    // The function first checks if the DocumentUrl property of the Pubs object contains certain keywords
+    // that indicate that the document is restricted. If the DocumentUrl contains any of these keywords,
+    // the function shows a Toast message indicating that the document is restricted and cannot be accessed.
+    // If the DocumentUrl does not contain any of the restricted keywords, the function creates an Intent to view
+    // the PDF document with a PDF viewer application. If an ActivityNotFoundException is caught, it means that there
+    // is no PDF viewer application installed on the device, so the function launches a PDF viewer activity
+    override fun onMainPubsClickListener(pubs: Pubs) {
+        try {
+            if (pubs.DocumentUrl?.contains("generic_restricted.pdf") == true
+                || (pubs.DocumentUrl?.contains("restricted_access.pdf")) == true
+                || (pubs.DocumentUrl?.contains("for_official_use_only.pdf")) == true
+                || (pubs.DocumentUrl?.contains("generic_fouo.pdf")) == true
+                || (pubs.DocumentUrl?.contains("stocked_and_issued")) == true
+                || (pubs.DocumentUrl?.contains("generic_opr1.pdf")) == true
+                || (pubs.DocumentUrl?.contains("generic_opr.pdf")) == true) {
+
+                showRestrictedToast(getString(R.string.pub_restricted))
+
+            } else {
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.setDataAndType(Uri.parse(pubs.DocumentUrl), "application/pdf")
+                startActivity(intent)
+            }
+
+            // Launches PDFViewer Library if no reader is installed
+            // Source: https://github.com/afreakyelf/Pdf-Viewer
+        } catch (e: ActivityNotFoundException) {
+            startActivity(
+                // Use 'launchPdfFromPath' if you want to use assets file (enable "fromAssets" flag) / internal directory
+                PdfViewerActivity.launchPdfFromUrl(     //PdfViewerActivity.Companion.launchPdfFromUrl(..   :: incase of JAVA
+                    requireContext(),
+                    "${pubs.DocumentUrl}",     // PDF URL in String format
+                    "${pubs.Title}",          // PDF Name/Title in String format
+                    "",                  // If nothing specific, Put "" it will save to Downloads
+                    enableDownload = true             // This param is true by default.
+                )
+            )
+        }
+    }
+
+    // Callback to refresh (show all) publications list when user selects back button
+    // or closes app after hitting back twice within 2 seconds
+    private fun closeOrRefreshApp() {
+        (activity as MainActivity).supportActionBar?.title = getString(R.string.app_home)
+        adapter?.filter?.filter("")
+        if (exit){
+            requireActivity().finish() // finish activity
+        }else{
+            Toasty.normal(requireContext(), getString(R.string.action_exit_app)).show()
+            exit = true
+            Handler(Looper.getMainLooper()).postDelayed({ exit = false }, 1 * 1000)
+        }
+    }
+
+    // Cleans up toast notification for restricted access publications
+    private fun showRestrictedToast(message: String) {
+        Toasty.error(requireContext(), message, Toast.LENGTH_SHORT, false).show()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}

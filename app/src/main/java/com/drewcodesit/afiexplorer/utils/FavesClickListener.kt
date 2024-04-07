5c6b9d704519528d91c5bf7a68307500123a2ba8
DrewStephensCoding <usafcrewchief06@yahoo.com>
2024-04-07 17:17:48 -0500

    Updated dependencies

--- app/src/main/java/com/drewcodesit/afiexplorer/utils/FavesClickListener.kt
+++
@@ -1,9 +0,0 @@
-package com.drewcodesit.afiexplorer.utils
-
-import com.drewcodesit.afiexplorer.database.FavoriteEntity
-
-interface FavesClickListener {
-
-    fun onFavesSelectedListener(faves: FavoriteEntity)
-    fun onFavesDeletedListener(faveSingleItemDelete: FavoriteEntity, position: Int)
-}

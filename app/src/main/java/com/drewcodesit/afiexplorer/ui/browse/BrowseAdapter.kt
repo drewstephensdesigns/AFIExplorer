5869f30b2a1d017a177641685337ef2ed1f697a5
andrew stephens <drewstephensdesigns@gmail.com>
2026-06-11 18:54:36 -0500

    UI improvements, CI/CD pipeline build, bug fixes

--- app/src/main/java/com/drewcodesit/afiexplorer/ui/browse/BrowseAdapter.kt
+++ app/src/main/java/com/drewcodesit/afiexplorer/ui/browse/BrowseAdapter.kt
@@ -1,7 +1,6 @@
 package com.drewcodesit.afiexplorer.ui.browse
 
 import android.content.Context
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Filter
